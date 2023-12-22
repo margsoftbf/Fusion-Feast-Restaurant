91419886105742c8f215b93bd91e76cbacd37ef2
Marcin <mgarski@tutanota.com>
2023-12-21 19:38:51 -0500

    Changes

--- postcss.config.js
+++ postcss.config.js
@@ -1,37 +1,6 @@
-const purgecss = require('@fullhuman/postcss-purgecss')({
-	content: [
-		'./pages/**/*.{js,jsx,ts,tsx}',
-		'./components/**/*.{js,jsx,ts,tsx}',
-	],
-	defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
-});
-
 module.exports = {
-	plugins: {
-		'postcss-flexbugs-fixes': {},
-		'postcss-preset-env': {
-			autoprefixer: {
-				flexbox: 'no-2009',
-			},
-			stage: 3,
-			features: {
-				'custom-properties': false,
-			},
-		},
-		tailwindcss: {},
-		autoprefixer: {},
-		...(process.env.NODE_ENV === 'production'
-			? {
-					'@fullhuman/postcss-purgecss': {
-						content: [
-							'./pages/**/*.{js,jsx,ts,tsx}',
-							'./components/**/*.{js,jsx,ts,tsx}',
-							// inne pliki, gdzie używasz CSS
-						],
-						defaultExtractor: (content) =>
-							content.match(/[\w-/:]+(?<!:)/g) || [],
-					},
-			  }
-			: {}),
-	},
-};
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+}
