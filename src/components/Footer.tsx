e72341b1bc60753ab7828db75346f8f993605ec7
Marcin <mgarski@tutanota.com>
2024-01-13 20:52:31 -0500

    Change duration

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -19,7 +19,7 @@ const Footer = () => {
 			visible: { opacity: 1, y: 0 },
 			hidden: { opacity: 0, y: 50 },
 		}}
-		transition={{ duration: 0.7, type: 'ease-in' }}>
+		transition={{ duration: 0.2, type: 'ease-in' }}>
 			<h2 id='footer-heading' className='sr-only'>
 				Footer
 			</h2>
